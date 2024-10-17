027a5ad8b367cb2d933d985b3bd1eabbc430eead
tyan0026 <tyan0026@student.monash.edu>
2024-10-17 23:56:41 +1100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:process.env.NODE_ENV === 'production'
+  ? '/FIT5032week12/'
+  : '/',
+  define: {
+    "process.env.NODE_ENV":JSON.stringify(process.env.NODE_ENV)
+  },
   plugins: [vue()],
 })
